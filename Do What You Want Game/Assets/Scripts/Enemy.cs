9bc97a343747703b1dff93a9cd633aea10e719f8
DurocIsCooking <durocher.tristan@gmail.com>
2021-09-15 22:11:07 -0400

    Enemy movement

--- Do What You Want Game/Assets/Scripts/Enemy.cs
+++ Do What You Want Game/Assets/Scripts/Enemy.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    // Used the following tutorial for having enemies rotate towards the player:
+    // https://www.youtube.com/watch?v=mKLp-2iseDc&ab_channel=KristerCederlund
+
+    // How fast enemy moves forward
+    private float _movementSpeed;
+
+    // Movement target is a child object in front of the enemy
+    // It is used to move the enemy forward regardless of its rotation.
+    private Transform _movementTarget;
+
+    private float _rotationSpeed;
+
+    private GameObject _player;
+
+    private void Start()
     {
-        
+        _movementTarget = gameObject.transform.GetChild(0);
+        _movementSpeed = EnemyManager.enemySpeed;
+        _rotationSpeed = EnemyManager.maximumEnemyRotation;
+        _player = EnemyManager.player;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        ManageMovement();
     }
+
+    private void ManageMovement()
+    {
+        // Move forward
+        transform.position = Vector3.MoveTowards(transform.position, _movementTarget.transform.position, _movementSpeed);
+
+        // Rotate towards player
+        Vector2 direction = _player.transform.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, _rotationSpeed * Time.deltaTime);
+
+    }
+
 }
--- Do What You Want Game/Assets/Scripts/EnemyManager.cs
+++ Do What You Want Game/Assets/Scripts/EnemyManager.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static float _enemySpeed;
+    private static float _enemyRotationSpeed;
+    private static GameObject _player;
+
+    public static float enemySpeed
     {
-        
+        get
+        {
+            return _enemySpeed;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static float maximumEnemyRotation
     {
-        
+        get
+        {
+            return _enemyRotationSpeed;
+        }
     }
+
+    public static GameObject player
+    {
+        get
+        {
+            return _player;
+        }
+    }
+
+    private static EnemyManager _instance;
+
+    public static EnemyManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                EnemyManager singleton = GameObject.FindObjectOfType<EnemyManager>();
+                if (singleton == null)
+                {
+                    GameObject go = new GameObject();
+                    _instance = go.AddComponent<EnemyManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    
+
+    private void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        _enemySpeed = 0.05f;
+        _enemyRotationSpeed = 0.8f;
+        _player = GameObject.Find("Player");
+    }
+
+    
 }
